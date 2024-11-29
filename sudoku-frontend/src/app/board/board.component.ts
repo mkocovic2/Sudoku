49d31cd687643a20250218759da2cc9f8d9d3796
mintuka <mintuka2015@gmail.com>
2024-11-29 16:29:44 -0500

    check

--- sudoku-frontend/src/app/board/board.component.ts
+++ sudoku-frontend/src/app/board/board.component.ts
@@ -8,8 +8,31 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 export class BoardComponent implements OnInit {
 
   boxes = Array(9)
-  board: number[][] = Array.from({ length: 9 }, () => Array(9).fill(0));
-  puzzleSolution: number[][] = Array.from({length: 9}, () => Array(9).fill(0))
+  // board: number[][] = Array.from({ length: 9 }, () => Array(9).fill(0));
+  // puzzleSolution: number[][] = Array.from({length: 9}, () => Array(9).fill(0))
+  board = [
+    [0, 0, 0, 0, 2, 3, 8, 0, 7],
+    [0, 7, 0, 0, 0, 0, 0, 5, 0],
+    [2, 3, 0, 0, 8, 7, 0, 4, 1],
+    [0, 0, 1, 3, 0, 8, 0, 0, 0],
+    [0, 0, 3, 7, 4, 0, 0, 1, 2],
+    [4, 0, 7, 1, 0, 0, 6, 0, 8],
+    [1, 0, 9, 0, 3, 0, 7, 8, 0],
+    [0, 0, 0, 8, 7, 0, 0, 0, 5],
+    [7, 4, 0, 9, 0, 0, 3, 0, 0]
+  ];
+  puzzleSolution = [
+    [9, 1, 4, 5, 2, 3, 8, 6, 7],
+    [8, 7, 6, 4, 9, 1, 2, 5, 3],
+    [2, 3, 5, 6, 8, 7, 9, 4, 1],
+    [5, 2, 1, 3, 6, 8, 4, 7, 9],
+    [6, 8, 3, 7, 4, 9, 5, 1, 2],
+    [4, 9, 7, 1, 5, 2, 6, 3, 8],
+    [1, 5, 9, 2, 3, 6, 7, 8, 4],
+    [3, 6, 2, 8, 7, 4, 1, 9, 5],
+    [7, 4, 8, 9, 1, 5, 3, 2, 6]
+  ];
+  
   clickedBoxPosition: number[] = [-1, -1]
   noteMode: boolean = false
   action: string = ''
@@ -33,6 +56,7 @@ export class BoardComponent implements OnInit {
   }
 
   setCell(value: number) {
+    this.action = ''
     const [x, y] = this.getPosition()
     
     if (x == -1 || y == -1) return
