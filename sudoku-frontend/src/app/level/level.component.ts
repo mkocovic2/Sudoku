f6431885a9f4ae34366a0c9aa567114fab9ca98c
mintuka <mintuka2015@gmail.com>
2024-11-30 06:04:14 -0500

    integrate

--- sudoku-frontend/src/app/level/level.component.ts
+++ sudoku-frontend/src/app/level/level.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../services/api.service';
+import { Router } from '@angular/router';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-level',
@@ -7,4 +10,28 @@ import { Component } from '@angular/core';
 })
 export class LevelComponent {
 
+  constructor(
+    private router: Router, 
+    private apiService: ApiService
+  ) {}
+
+  startPuzzle(difficulty: string) {
+      const game = { 
+        difficulty,
+        user_id: this.generateUserId() 
+      }
+      this.apiService.startPuzzle(game).pipe(take(1)).subscribe({
+        next: (res: any) => {
+          if (Object.keys(res).length > 0) {
+            this.apiService.updateBoard({board: res?.initial_grid, session_id: res?.session_id})
+            this.router.navigate(['/board'], {
+              queryParams: {difficulty: difficulty, user: game.user_id}
+            })
+          }
+        }
+      })
+  }
+  generateUserId() {
+    return Math.random().toString(36).substr(2, 9);
+  }
 }
