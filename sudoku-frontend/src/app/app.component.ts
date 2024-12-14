e73112c819074bcdf8df25b8a52a918a11558d40
Mintesnot Kassa <59524273+Mintuka@users.noreply.github.com>
2024-12-14 00:06:58 -0500

    document code (#10)

--- sudoku-frontend/src/app/app.component.ts
+++ sudoku-frontend/src/app/app.component.ts
@@ -8,3 +8,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'sudoku-frontend';
 }
+
+/*
+Main contributor - Mintesnot
+  -Generated the components, services, styles and assets
+  -Worked on most part of the frontend development
+  -Designed main page, game/board page and congradulations page
+  -Implented with seamless user experience
+
+Assistant contributors - Michael and Danny
+  -Ensured every button, cell and timer is successfully integrated with backend
+  -Designed logo and background image
+  -Helped on improving the user experience
+*/
--- sudoku-frontend/src/app/services/api.service.ts
+++ sudoku-frontend/src/app/services/api.service.ts
@@ -13,39 +13,44 @@ export class ApiService {
   boardCache = new BehaviorSubject<any>({})
   boardObs = this.boardCache.asObservable()
   
+  //API service to start a puzzle
   startPuzzle(data:any) {
     const path = `${environment.baseUrl}/start_puzzle`;
     return this.http.post(path, data)
   }
 
+  //API service to update each cell move
   makeMove(data:any) {
     const path = `${environment.baseUrl}/process_move`;
     return this.http.post(path, data)
   }
 
+  //API service to check if game is compeleted successfully
   checkSolution(data:any) {
     const path = `${environment.baseUrl}/check_solution`;
     return this.http.post(path, data)
   }
 
+  //API service to get incorrect moves
   getIncorrectMoves(data: any) {
     const path = `${environment.baseUrl}/store_incorrect_moves`;
     return this.http.post(path, data)
   }
   
+  //API service to undo last move only
   undoMove(data: any) {
     const path = `${environment.baseUrl}/undo_move`;
     return this.http.post(path, data)
   }
 
+  //API service to return back the puzzle to the state where all moves are correct
   undoUntilCorrect(data: any) {
     const path = `${environment.baseUrl}/undo_until_correct`;
     return this.http.post(path, data)
   }
 
+  //API service to update the time the user stopped playing
   updateTime(data: any) {
-    // const path = `${environment.baseUrl}/save_time`;
-    // return this.http.post(path, data)
     const endpoint = `${environment.baseUrl}/save_time`;
   
     fetch(endpoint, {
@@ -56,16 +61,19 @@ export class ApiService {
     });
   }
 
+  //API service to get both random and specific hint
   getHint(data: any) {
     const path = `${environment.baseUrl}/get_hint`;
     return this.http.post(path, data)
   }
 
+  //API service to load a specific board with boardId given
   loadBoard(data: any) {
     const path = `${environment.baseUrl}/load_board`;
     return this.http.post(path, data)
   }
 
+  //Observer initialized to pass the board value from one route to another route
   updateBoard(board: any) {
     this.boardCache.next(board)
   }
